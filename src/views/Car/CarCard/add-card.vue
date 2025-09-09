7ade6ce8cbef76e12f0ffbd855a313b221c92c18
songzhibing-yyy <sesongzhibing@mail.scut.edu.cn>
2025-09-09 22:29:10 +0800

    feat: step2

--- src/views/Car/CarCard/add-card.vue
+++ src/views/Car/CarCard/add-card.vue
@@ -7,18 +7,23 @@
       <div class="form-container">
         <div class="title">车辆信息</div>
         <div class="form">
-          <el-form label-width="100px">
-            <el-form-item label="车主姓名">
-              <el-input />
+          <!--
+            el-form: :model :rules
+            el-form-item prop指定用哪条规则
+            el-input：v-model双向绑定
+          -->
+          <el-form label-width="100px" :model="carInfoForm" :rules="carInfoRules">
+            <el-form-item label="车主姓名" prop="personName">
+              <el-input v-model="carInfoForm.personName" />
             </el-form-item>
-            <el-form-item label="联系方式">
-              <el-input />
+            <el-form-item label="联系方式" prop="phoneNumber">
+              <el-input v-model="carInfoForm.phoneNumber" />
             </el-form-item>
-            <el-form-item label="车辆号码">
-              <el-input />
+            <el-form-item label="车辆号码" prop="carNumber">
+              <el-input v-model="carInfoForm.carNumber" />
             </el-form-item>
-            <el-form-item label="企业联系人">
-              <el-input />
+            <el-form-item label="车辆品牌" prop="carBrand">
+              <el-input v-model="carInfoForm.carBrand" />
             </el-form-item>
           </el-form>
         </div>
@@ -59,6 +64,54 @@
 
 <script>
 export default {
+  data() {
+    const validatorCarNumber = (rule, value, callback) => {
+      // value: 用户在表单里输入的最新的数据
+      // callback 是一个放行函数 校验通过直接调用，如果未通过callback(new Error('错误提示'))
+      const plateNumberRegex = /^[\u4E00-\u9FA5][\da-zA-Z]{6}$/
+      if (plateNumberRegex.test(value)) {
+        callback()
+      } else {
+        callback(new Error('请输入符合规范的车牌号'))
+      }
+    }
+    return {
+      // 表单对象
+      carInfoForm: {
+        personName: '',
+        phoneNumber: '',
+        carNumber: '',
+        carBrand: ''
+      },
+      // 规则对象
+      carInfoRules: {
+        personName: [{
+          required: true,
+          message: '请输入车主姓名',
+          trigger: 'blur'
+        }],
+        phoneNumber: [{
+          required: true,
+          message: '请输入联系方式',
+          trigger: 'blur'
+        }],
+        carNumber: [{
+          required: true,
+          message: '请输入车辆号码',
+          trigger: 'blur'
+        },
+        {
+          validator: validatorCarNumber,
+          trigger: 'blur'
+        }],
+        carBrand: [{
+          required: true,
+          message: '请输入车辆品牌',
+          trigger: 'blur'
+        }]
+      }
+    }
+  }
 
 }
 </script>
