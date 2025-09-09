b484d4e84ca2153e2f0e1dcc0817e1b18d9e0ca3
songzhibing-yyy <sesongzhibing@mail.scut.edu.cn>
2025-09-09 11:42:11 +0800

    feat: Token持久化

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,15 +1,19 @@
 import { loginAPI } from '@/api/user'
+import { setToken, getToken } from '@/utils/auth'
 export default {
   namespaced: true,
   // 数据状态 响应式 data
   state: {
-    token: ''
+    token: getToken() || ''// 初始化优先从本地取
 
   },
   // 同步修改Vuex架构中 有且只有一种提交mutation
   mutations: {
     setToken(state, newToken) {
+      // Vuex存一份
       state.token = newToken
+      // Cookie也存一份
+      setToken(newToken)
     }
   },
   // 异步 接口请求+提交mutation
